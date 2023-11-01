335f89cc21c5c31d4d7ab2f522978aefee12191f
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-11-01 13:51:50 +0100

    fetching history works

--- Common/JS/app.js
+++ Common/JS/app.js
@@ -1,18 +1,19 @@
-let myDate = new Date();
-let currentYear = myDate.getFullYear();
-let currentMonth = myDate.getMonth() + 1;
-let currentDay = myDate.getDate();
-const currentHours = myDate.getHours() + 1;
+// Getting current and next hour
 
-const indexContent = document.getElementById("indexContent");
-// const currentTime = document.getElementById("time");
-const currentTime = document.getElementById("currentTime");
+let currentHours = new Date().getHours();
+let currentHours2 = new Date().getHours() + 1;
 
+// Fetching the data for the current date
 
-fetchTodaysData();
-
-function fetchTodaysData() {
-  let URL = `https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDay}_DK1.json`;
+function fetched() {
+  let currentYear = new Date().getFullYear();
+  let currentMonth = new Date().getMonth() + 1;
+  let currentDay = new Date()
+    .toLocaleDateString(undefined, {
+      day: "2-digit",
+    })
+    .replace(".", "");
+  let URL = `https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDay}_DK2.json`;
 
   fetch(URL)
     .then((res) => res.json())
@@ -24,22 +25,16 @@ function fetchTodaysData() {
     });
 }
 
+// Displaying the fetched data
 function showToday(data) {
-    console.log("Data", data);
-  
-    // We get the current hour in the 24-hour format
-    const currentHour = myDate.getHours();
+  const indexContent = document.getElementById("indexContent");
+  const currentTime = document.getElementById("currentTime");
+  console.log("Data", data);
+  indexContent.innerHTML = `<p>${
+    Math.round(data[currentHours].DKK_per_kWh * 1000) / 1000
+  } kr </br> <span>pr. kwh</span></p>
+  `;
+  currentTime.innerHTML = currentHours + ".00" + " - " + currentHours2 + ".00";
+}
 
-      // Calculating the next hour
-  const nextHour = (currentHour + 1) % 24;
-  
-    // Format the current time as HH:mm (in this case as 0)
-    const formattedTime = `${currentHour < 10 ? "0" : ""}${currentHour}:00 - ${nextHour < 10 ? "0" : ""}${nextHour}:00`;
-  
-    // Displaying the current electricity price and time
-    indexContent.innerHTML = `<p>${Math.round(data[currentHour].DKK_per_kWh * 1000) / 1000} kr</p>
-    <p>pr. kwh</p>`;
-    
-    // Displaying the current time
-    currentTime.textContent = `Current Time: ${formattedTime}`;
-  }
+fetched();
---
+++ Common/JS/history_script.js
@@ -0,0 +1,71 @@
+function fetchHistoricalPrices() {
+  // We get the selected date from the input field
+  const selectedDate = document.getElementById("start").value;
+  
+  // Extract year, month, and day from the selected date
+  const [selectedYear, selectedMonth, selectedDay] = selectedDate.split("-");
+    const URL = `https://www.elprisenligenu.dk/api/v1/prices/${selectedYear}/${selectedMonth}-${selectedDay}_DK2.json`;
+
+  // Fetching historical electricity prices
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      displayHistoricalPrices(data);
+      console.log(data)
+    })
+    .catch((error) => {
+      console.error("Error fetching historical prices:", error);
+    });
+}
+
+function displayTodaysPrices(data) {
+  const todaysElPrices = document.getElementById("todaysElPrices");
+  let priceHTML = "<h3>Latest prices for today:</h3>";
+
+  // Assuming that 'data' contains an array of prices for today
+  for (let hour = 0; hour < data.length; hour++) {
+    const price = Math.round(data[hour].DKK_per_kWh * 1000) / 1000;
+    const currentHour = hour < 10 ? `0${hour}` : `${hour}`;
+    priceHTML += `${currentHour}:00: ${price} kr per kWh<br>`;
+  }
+
+  todaysElPrices.innerHTML = priceHTML;
+}
+
+
+function pickDate() {
+  let currentYear = new Date().getFullYear();
+  let currentMonth = new Date().getMonth() + 1;
+  let currentDay = new Date()
+    .toLocaleDateString(undefined, {
+      day: "2-digit",
+    })
+    .replace(".", "");
+  let URL = `https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDay}_DK2.json`;
+
+  const dateControl = document.querySelector('input[type="date"]');
+  dateControl.value = `${currentYear}-${currentMonth}-${currentDay}`;
+  dateControl.max = `${currentYear}-${currentMonth}-${currentDay}`;
+}
+
+
+function displayHistoricalPrices(data) {
+  // Using data to display historical prices
+  const todaysElPrices = document.getElementById("todaysElPrices");
+
+  let priceHTML = "<h3>Historical prices for the selected date:</h3>";
+
+  for (let hour = 0; hour < data.length; hour++) {
+    const price = Math.round(data[hour].DKK_per_kWh * 1000) / 1000;
+    const currentHour = hour < 10 ? `0${hour}` : `${hour}`;
+    priceHTML += `${currentHour}:00: ${price} kr per kWh<br>`;
+  }
+
+  todaysElPrices.innerHTML = priceHTML;
+}
+
+// Attach an event listener to the date input
+const dateInput = document.getElementById("start");
+dateInput.addEventListener("change", fetchHistoricalPrices);
+// Initialize with today's date
+pickDate();
